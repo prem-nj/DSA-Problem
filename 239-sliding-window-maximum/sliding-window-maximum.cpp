0949d55ed942ad3510de241ad954bdb17a5a2a56
PREM NAVNATH JADHAV <prembahi89@gmail.com>
2025-04-01 16:16:16 +0530

    Time: 21 ms (70.16%) | Memory: 139.1 MB (62.20%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,22 +2,36 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        int n=nums.size();
-        deque<int>deq;
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            while(!deq.empty() && nums[deq.back()]<nums[i]){
+        int n = nums.size();
+        deque<int> deq;
+        vector<int> ans;
+        int j = 0, i = 0;
+
+        while (j < n) {
+            // Remove smaller elements from the back
+            while (!deq.empty() && nums[deq.back()] < nums[j]) {
                 deq.pop_back();
             }
-            deq.push_back(i);
-            if (deq.front() < i - k + 1) {
-                deq.pop_front();
+
+            // Push current index
+            deq.push_back(j);
+
+            // Maintain window size
+            if (j - i + 1 > k) {
+                if (deq.front() == i) {
+                    deq.pop_front();
+                }
+                i++;  // Slide the window
             }
-            if(i>=k-1){
+
+            // Collect max when window size reaches `k`
+            if (j - i + 1 == k) {
                 ans.push_back(nums[deq.front()]);
             }
+
+            j++;  // Expand the window
         }
-    return ans;
+
+        return ans;
     }
 };
