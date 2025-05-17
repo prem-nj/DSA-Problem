f1606be51d812fe6e0b728b9bdbcec795bed0baa
PREM NAVNATH JADHAV <prembahi89@gmail.com>
2025-05-17 20:36:28 +0530

    Time: 54 ms (30.33%) | Memory: 90.7 MB (36.47%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>set;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(set.find(nums[i])!=set.end()){
+                return true;
+            }
+            set.insert(nums[i]);
+        }
+        return false;
+    }
+};
