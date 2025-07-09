f200e0a2bc2955a0638baa1c894de216f21fc4a7
PREM NAVNATH JADHAV <prembahi89@gmail.com>
2025-07-09 16:54:39 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (93.57%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& arr) {
+    vector<int> majorityElement(vector<int>& nums) {
         vector<int>res;
-        int n=arr.size();
-        unordered_map<int,int>count;
-      for(int el :arr){
-          count[el]++;
-      }
-        for(auto  i:count){
-            if(i.second>n/3){
-                res.push_back(i.first);
+        int n=nums.size();
+        int maj1=-1;
+        int maj2=-1;
+        int count1=0;
+        int count2=0;
+        for(int ele:nums){
+            if(ele==maj1){
+                count1++;
+            }else if(ele==maj2){
+                count2++;
+            }else if(count1==0){
+                count1=1;
+                maj1=ele;
+            }else if(count2==0){
+                count2=1;
+                maj2=ele;
+            }else{
+                count1--;
+                count2--;
+            }
+            
+        }
+        int cnt1=0 ,cnt2=0;
+        for(int ele:nums){
+            if(ele==maj1){
+               cnt1++;
+            }else if(ele==maj2){
+                cnt2++;
             }
         }
-       // if (res.size() == 2 && res[0] > res[1])
-       //  swap(res[0], res[1]);
-        sort(arr.begin(),arr.end());
+        if(cnt1>n/3){
+            
+            res.push_back(maj1);
+        } 
+        if (cnt2>n/3 && maj1!=maj2){
+            res.push_back(maj2);
+        }
+        if(res.size()==2 && res[0]>res[1]){
+            swap(res[0],res[1]);
+        }
         return res;
-        
     }
 };
