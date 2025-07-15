f9e91b5c8d837344edf8ab5fef4249ef26b1296c
PREM NAVNATH JADHAV <prembahi89@gmail.com>
2025-07-15 14:15:57 +0530

    Time: 21 ms (30.09%) | Memory: 15.6 MB (17.39%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-bool allzero( vector<int>&counter)
-{
-for(int i=0;i<counter.size();i++){
-    if(counter[i]!=0){
-        return false;
-    }
-}
-
-return true;}
     vector<int> findAnagrams(string s, string p) {
+        int n=s.size();
+        int m=p.size();
+        vector<int>cnt;
+        unordered_map<char,int>patfreq,winfreq;
+        for(char &ch:p){
+            patfreq[ch]++;
+        }
         int i=0;
         int j=0;
-        vector<int>ans;
-        vector<int>counter(26,0);
-        for(int i=0;i<p.length();i++){
-            char ch=p[i];
-            counter[ch-'a']++;
-        }
-        int n=s.length();
         while(j<n){
-            counter[s[j]-'a']--;
-            if(j-i+1==p.length()){
-                if(allzero(counter)){
-                    ans.push_back(i);
+            winfreq[s[j]]++;
+            if(j-i+1==m){
+                if(winfreq==patfreq){
+                    cnt.push_back(i);
                 }
-                counter[s[i]-'a']++;
-                i++;
+            winfreq[s[i]]--;
+
+               if (winfreq[s[i]] == 0) {
+                    winfreq.erase(s[i]);
+                }
+                
+            i++;
             }
-       j++; }
-   return ans; }
+            j++;
+        }
+        return cnt;
+    }
 };
