b5480e714d1f9da4be6788e3f5696be5bc514d01
PREM NAVNATH JADHAV <prembahi89@gmail.com>
2025-07-15 22:05:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -1,61 +1,29 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
     int longestKSubstr(string &s, int k) {
-        // your code here
+        // code hereint 
+        int j=0,i=0;
         int n=s.size();
-        int j=0;
-        int i=0;
-        unordered_map<char,int>map;
-        int maxi=INT_MIN;
+        int maxi=-1;
+ unordered_map<char,int>count;
         while(j<n){
-            map[s[j]]++;
-        
-            if(map.size()==k){
-                maxi=max(j-i+1,maxi);
-                
-          
-            }else if(map.size()>k){
-                while(map.size()>k){
-                    map[s[i]]--;
-                if(map[s[i]]==0){
-                    map.erase(s[i]);
-                }
+            count[s[j]]++;
+            if(count.size()==k){
+                maxi=max(maxi,j-i+1);
+            }else if(count.size()>k){
+                while(count.size()>k){
+                    count[s[i]]--;
+                    if(count[s[i]]==0){
+                        count.erase(s[i]);
+                    }
                     i++;
                 }
             }
-            j++;
+                j++;
+            
+            
         }
-   return (maxi==INT_MIN) ? -1 : maxi;
-   }
-};
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        int k;
-        cin >> k;
-        Solution ob;
-        cout << ob.longestKSubstr(s, k) << endl;
-
-        cout << "~"
-             << "\n";
+return maxi;
+        
     }
-}
-
-// } Driver Code Ends
+};
