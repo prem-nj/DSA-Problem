6abf07f959983ffbad37d54528ae659dad09ada6
PREM NAVNATH JADHAV <prembahi89@gmail.com>
2025-08-01 23:19:38 +0530

    Time: 2 ms (41.25%) | Memory: 16.8 MB (6.35%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,26 +1,24 @@
 class Solution {
     private:
     void solve(vector<int>nums,vector<int>output,int index,vector<vector<int>>&ans){
-        //base case
         if(index>=nums.size()){
             ans.push_back(output);
             return ;
         }
-        //exclude
+ ///exclude
         solve(nums,output,index+1,ans);
-       //include
-       int elem=nums[index];
-       output.push_back(elem);
-       solve(nums,output,index+1,ans);
-
+        //include
+        int elem=nums[index];
+        output.push_back(elem);
+        solve(nums,output,index+1,ans);
+        
     }
-
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>output;
         int index=0;
-      solve(nums,output,index,ans);
-      return ans;
+        solve(nums,output,index,ans);
+        return ans;
     }
 };
