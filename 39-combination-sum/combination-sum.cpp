ca98efdf77eecce12cc7529fcace9f18cda09fb2
PREM NAVNATH JADHAV <prembahi89@gmail.com>
2025-09-02 21:02:53 +0530

    Time: 2 ms (60.95%) | Memory: 13.9 MB (90.73%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,41 +1,26 @@
 class Solution {
-private:
-set<vector<int>>s;
-  void solve(vector<int>&candidates,int  target,int index,vector<int>&cobn,vector<vector<int>>&ans){
-
+public:
+void cobsum(vector<int>&candidate,int index,int target,vector<vector<int>>&ans,vector<int>&cobn){
     //base case
-    if(index>=candidates.size() || target<0){
-      
-        return ;
+    if(index>=candidate.size() || target<0){
+      return;
     }
     if(target==0){
-        if(s.find(cobn)==s.end()){
-
-        ans.push_back(cobn);
-        s.insert(cobn);
-        }
-        return;
+        return ans.push_back(cobn);
     }
-    
-    //include
-    int num=candidates[index];
-    cobn.push_back(num);
-    solve(candidates,target-num,index+1,cobn,ans);
-    
-    //multiple
-    solve(candidates,target-num,index,cobn,ans);
-    
-    //exclude
+if(candidate[index]<=target){
+    cobn.push_back(candidate[index]);
+    cobsum(candidate,index,target-candidate[index],ans,cobn);
     cobn.pop_back();
-    solve(candidates,target,index+1,cobn,ans);
-    
-  }
-public:
+}
+//exclude
+cobsum(candidate,index+1,target,ans,cobn);
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-     vector<vector<int>>ans;
-     vector<int>cobn;
         int index=0;
-        solve(candidates,target,index,cobn,ans);
+        vector<vector<int>>ans;
+        vector<int>cobn;
+        cobsum(candidates,index,target,ans,cobn);
         return ans;
     }
 };
