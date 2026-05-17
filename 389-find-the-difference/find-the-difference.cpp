0864750997cd469cc57efc88f6759f04061589b4
PREM NAVNATH JADHAV <premjadhav21314@gmail.com>
2026-05-17 16:58:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (75.83%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int>freq(26,0);
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
+        }
+        for(int j=0;j<t.length();j++){
+            freq[t[j]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return i+'a';
+            }
+        }
+        return  ' ';
+    }
+};
