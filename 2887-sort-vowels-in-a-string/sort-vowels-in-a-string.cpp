723a14d5ec62e9fd9e6d4c9a1f9f5dd83915e7e2
PREM NAVNATH JADHAV <premjadhav21314@gmail.com>
2026-06-09 19:10:18 +0530

    Time: 15 ms (60.34%) | Memory: 16.7 MB (29.87%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        //upper
+        vector<int>upper(26,0);
+        vector<int>lower(26,0);
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+                lower[s[i]-'a']++;
+                s[i]='#';
+
+            }else if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+                 upper[s[i]-'A']++;
+                 s[i]='#';
+            }
+        }
+        string vowel;
+        for(int i=0;i<26;i++){
+            char ch='A'+i;
+            while(upper[i]){
+                vowel+=ch;
+                upper[i]--;
+            }
+            
+        
+        }
+
+        for(int i=0;i<26;i++){
+            char c='a'+i;
+            while(lower[i]){
+                vowel+=c;
+                lower[i]--;
+            }
+        }
+
+        int pv=0,ps=0;
+        while(pv<vowel.size()){
+
+        if(s[ps]=='#'){
+            s[ps]=vowel[pv++];
+            
+        }
+         ps++;
+        }
+        return s;
+    }
+
+};
