fa445e60c85899002a13b2d612c5fff156b56c54
PREM NAVNATH JADHAV <premjadhav21314@gmail.com>
2026-06-30 21:59:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (51.49%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    bool allcapital(string word){
-        for(char &ch:word){
-            if(ch<'A' || ch>'Z'){
-                return false;
-            }
-
-        }
-   return true;
-    }
-
-    bool allsmall(string word){
-        for(char &ch:word){
-            if(ch<'a' || ch>'z'){
-                return false;
-            }
-        }
-        return true;
-    }
-
     bool detectCapitalUse(string word) {
-
-      if(allcapital(word)|| allsmall(word)||allsmall(word.substr(1)) ){
-        return true;
-      }
-      return false;
+        int countcap=0;
+        for(char ch:word){
+            if(isupper(ch)){
+                countcap++;
+            }
+        }
+        if(countcap==word.length() || countcap==0 ||(countcap==1 && isupper(word[0]))){
+            return true;
+        }
+        return false;
     }
 };
