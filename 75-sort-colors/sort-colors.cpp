0f0d8c85dae40dfb683879090e69596325059fca
PREM NAVNATH JADHAV <premjadhav21314@gmail.com>
2026-07-03 12:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.38%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-sort(nums.begin(),nums.end());
+     
+     int c0=0;
+     int c1=0;
+     int n=nums.size();
+     int c2=0;
 
+     for(int i=0;i<n;i++){
+        if(nums[i]==0){
+            c0++;
+        }else if(nums[i]==1){
+            c1++;
+        }else{
+            c2++;
         }
+     }
+     int i=0;
+    while(c0--){
+        nums[i++]=0;
+    } 
+     while(c1--){
+        nums[i++]=1;
+    }
+      while(c2--){
+        nums[i++]=2;
+    }
+    }
+
 };
