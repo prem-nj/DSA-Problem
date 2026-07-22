44733d19f015e496802f4dd0e5021be3431dd3bd
PREM NAVNATH JADHAV <premjadhav21314@gmail.com>
2026-07-22 16:19:48 +0530

    Time: 9 ms (55.20%) | Memory: 12 MB (23.85%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,14 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode*slow=head;
-        ListNode*fast=head;
-        while(fast!=NULL && fast->next !=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast){
-                return true;
-            }
+        ListNode*s=head;
+        ListNode*f=head;
+        while(f!=NULL && f->next!=NULL){
+        s=s->next;
+        f=f->next->next;
+        if(s==f){
+            return true;
         }
-    return false;}
+        }
+        return false;
+    }
 };
